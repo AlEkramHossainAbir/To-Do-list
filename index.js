dbc3bd956abc774fa4c43281889ceb9e6efaa97a
Al Ekram Hossain Abir <alabir65@gmail.com>
2020-06-10 13:54:25 +0600

    Current Time added

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+
 var show = document.getElementById("show");
-var inputbox = document.getElementById("inputbox");
-input.addEventListener("click",function(){
-    alert("check");
+var input = document.getElementById("input");
+
+let time = document.getElementById("date");
+let today = new Date();
+let month = today.getMonth();
+let date = today.getDate();
+let year = today.getFullYear();
+let output = `${date}/${month+1}/${year}`;
+time.innerHTML = output;
+
+
+input.addEventListener("keypress",function(task){
+        if(task.keyCode==13){
+            alert("ok");
+        }
+        
 })
+
+// input.addEventListener("click",function(){
+//     alert("check");
+// })
