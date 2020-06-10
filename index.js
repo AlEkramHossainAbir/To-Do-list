8ce2ee934595e48e04e41a6ceb2126f0f1640b9e
Al Ekram Hossain Abir <alabir65@gmail.com>
2020-06-10 21:33:57 +0600

    Date Updated, Clear added, list updated, Checkboxed Changed, trash style changed

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 let LIST = [];
-let id = 0;
-let position = "beforeend";
+let id=0;
+var store;
+
 var show = document.getElementById("show");
 var input = document.getElementById("input");
 let list = document.getElementById("list");
 let time = document.getElementById("date");
-let restart = document.getElementById("restart");
+let clear = document.getElementById("clear");
+//let del = document.getElementsByClassName("fa-trash-o");
+let date = {weekday: "long" , month: "short" , day: "numeric"};
 let today = new Date();
-let month = today.getMonth();
-let date = today.getDate();
-let year = today.getFullYear();
-let output = `${date}/${month + 1}/${year}`;
-time.innerHTML = output;
+const CHECK = "fa-check-circle";
+const UNCHECK ="fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+// let month = today.getMonth();
+// let date = today.getDate();
+// let year = today.getFullYear();
+// let output = `${date}/${month + 1}/${year}`;
+// time.innerHTML = output;
+time.innerHTML = today.toLocaleDateString("en-US",date);
+
+
+addToDo = (toDo,id, done, trash) =>{
+    if(trash){return;}
+    const DONE = done ? CHECK: UNCHECK;
+    const LINE = done ? LINE_THROUGH : " ";
+    let text =  `<li><i class="fa ${DONE}" job="complete" id="${id}"></i><span class="text ${LINE}">${toDo}</span><i class="fa fa-trash-o de" id="${id}" job="delete" onclick="del()"></i></li>`;
+    let position = "beforeend";
+    list.insertAdjacentHTML(position,text);
+} 
 
 
 input.addEventListener("keypress", (task) =>{
     if (task.keyCode == 13) {
-        var store = input.value;
+         store = input.value;
         if (store) {
-            list.innerHTML = `<li><input type="checkbox" id="radio"><span>${store}</span><i class="fas fa-trash"></i></li>`;
+            addToDo(store,id,false,false); 
+            LIST.push({
+                name: store,
+                id:id,
+                done: false,
+                trash:false
+            });
         }
         input.value = "";
+        id++;
 
     }
     
 })
-
-restart.addEventListener("click", () =>{
-    alert("ok");
+clear.addEventListener("click",()=>{
+    list.innerHTML= "";
 })
+
+del =() =>{
+    alert("del");
+}
