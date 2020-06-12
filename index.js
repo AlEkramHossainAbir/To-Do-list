112b4d1c762eded6c73ac93da6f1cc84afa0c76b
Al Ekram Hossain Abir <alabir65@gmail.com>
2020-06-12 20:39:47 +0600

    Line reduced

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let LIST = [];
+// let LIST = [];
 let id=0;
 var store;
-
 var show = document.getElementById("show");
 var input = document.getElementById("input");
 let list = document.getElementById("list");
@@ -9,9 +8,6 @@ let time = document.getElementById("date");
 let clear = document.getElementById("clear");
 let date = {weekday: "long" , month: "short" , day: "numeric" , year: "numeric"};
 let today = new Date();
-const CHECK = document.getElementsByClassName("fa-check-circle");
-const UNCHECK =document.getElementsByClassName("fa-circle-thin");
-const LINE_THROUGH = "lineThrough";
 // let month = today.getMonth();
 // let date = today.getDate();
 // let year = today.getFullYear();
@@ -20,8 +16,8 @@ const LINE_THROUGH = "lineThrough";
 time.innerHTML = today.toLocaleDateString("en-US",date);
 let text;
 
-addToDo = (toDo,id, done, trash) =>{
-   text =  `<li id="ok ${id}" ><i class="fa fa-circle-thin" job="complete" id="${id}"></i><span class="text lineThrough">${toDo}</span><i class="fa fa-trash-o de" id="${id}" job="delete" onclick="del(id,text)"></i></li>`;
+addToDo = (toDo,id) =>{
+   text =  `<li id="ok ${id}" ><i class="fa fa-circle-thin" id="${id}"></i><span>${toDo}</span><i class="fa fa-trash-o de" id="${id}" onclick="del(id)"></i></li>`;
     let position = "beforeend";
     list.insertAdjacentHTML(position,text);
 } 
@@ -31,7 +27,7 @@ input.addEventListener("keypress", (task) =>{
     if (task.keyCode == 13) {
          store = input.value;
         if (store) {
-            addToDo(store,id,false,false); 
+            addToDo(store,id); 
             // LIST.push({
             //     name: store,
             //     id:id,
